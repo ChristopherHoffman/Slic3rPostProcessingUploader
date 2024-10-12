d36dcbdb9a6d52b5b6eb6e257987bfe3ea3e5fbf
Christopher Hoffman <csh.hoffman@gmail.com>
2024-10-12 10:07:10 -0400

    Add snapshot testing to the project.

--- Slic3rPostProcessingUploaderUnitTests/Services/Parsers/OrcaParserTests.cs
+++ Slic3rPostProcessingUploaderUnitTests/Services/Parsers/OrcaParserTests.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Slic3rPostProcessingUploader.Services.Parsers;
+using Snapshooter.MSTest;
+
 
 namespace Slic3rPostProcessingUploaderUnitTests.Services.Parsers
 {
@@ -30,13 +32,8 @@ namespace Slic3rPostProcessingUploaderUnitTests.Services.Parsers
         {
             var parser = new OrcaParser();
             var result = parser.ParseGcode(OrcaParserTestGcode.CalibrationCube);
-            Assert.IsNotNull(result);
-            Assert.IsNotNull(result.settings);
-            Assert.AreEqual(1319, result.settings.estimated_print_time_seconds);
-            Assert.AreEqual(0, result.settings.material_used_mg);
-            Assert.AreEqual("Settings:", result.settings.note);
-            Assert.AreEqual("OrcaSlicer", result.Slicer);
-            Assert.AreEqual("2.2.0-rc", result.CuraVersion);
+
+            Snapshot.Match(result);
         }
 
     }
