df65850932cb7534aa96b47c4986c112a5ed2233
Christopher Hoffman <CSH.Hoffman@gmail.com>
2025-01-03 22:33:05 -0500

    feat: add support for Bambu Studio

---
+++ Slic3rPostProcessingUploaderUnitTests/Services/Parsers/BambuStudio/BambuStudioParserTests.cs
@@ -0,0 +1,85 @@
+﻿using Slic3rPostProcessingUploader.Services.Parsers.BambuStudio;
+using Snapshooter.MSTest;
+
+
+namespace Slic3rPostProcessingUploaderUnitTests.Services.Parsers.BambuStudio
+{
+    [TestClass]
+    public class BambuStudioParserTests
+    {
+        [TestMethod]
+        public void ShouldReturnAnEmptySettingWhenGcodeIsEmpty()
+        {
+            var parser = new BambuStudioParser("");
+            var result = parser.ParseGcode("");
+            Assert.IsNotNull(result);
+
+            Snapshot.Match(result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnExpectedValuesWhenGivenFullGcode()
+        {
+            var parser = new BambuStudioParser("");
+            var result = parser.ParseGcode(BambuStudioParserTestGcode.CalibrationCube);
+
+            Snapshot.Match(result);
+        }
+
+        [TestMethod]
+        public void ShouldRenderTheExpectedNoteWhenGivenATemplateWithNoReplacements()
+        {
+            string template = "Settings:";
+
+            var parser = new BambuStudioParser(template);
+            var result = parser.ParseGcode(BambuStudioParserTestGcode.CalibrationCube);
+
+            Assert.AreEqual("Settings:", result.settings.note);
+        }
+
+        [TestMethod]
+        public void ShouldRenderTheExpectedNoteWhenGivenATemplateWithASingleReplacement()
+        {
+            string template = """
+                Settings:
+                    Layer Height: {{layer_height}}
+                """;
+
+            var parser = new BambuStudioParser(template);
+            var result = parser.ParseGcode(BambuStudioParserTestGcode.CalibrationCube);
+
+            Assert.AreEqual("""
+                Settings:
+                    Layer Height: 0.2
+                """, result.settings.note);
+        }
+
+        [TestMethod]
+        public void ShouldRenderTheExpectedNoteWhenGivenATemplateWithMultipleReplacements()
+        {
+            string template = """
+                Settings:
+                    Layer Height: {{layer_height}}
+                    First Layer Height: {{initial_layer_print_height}}
+                    Wall Loops: {{wall_loops}}
+                    Top Shell Layers: {{top_shell_layers}}
+                    Bottom Shell Layers: {{bottom_shell_layers}}
+                    Sparse Infill Density: {{sparse_infill_density}}
+                """;
+
+            var parser = new BambuStudioParser(template);
+            var result = parser.ParseGcode(BambuStudioParserTestGcode.CalibrationCube);
+
+            Assert.AreEqual("""
+                Settings:
+                    Layer Height: 0.2
+                    First Layer Height: 0.28
+                    Wall Loops: 3
+                    Top Shell Layers: 3
+                    Bottom Shell Layers: 3
+                    Sparse Infill Density: 10%
+                """, result.settings.note);
+        }
+
+    }
+}
